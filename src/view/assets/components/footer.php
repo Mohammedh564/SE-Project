69d48b0910f03608ef276b8d7207acb817209fcc
DORMODO <ymb01072005@gmail.com>
2025-05-08 23:03:30 +0300

    some UIs

---
+++ src/view/assets/components/footer.php
@@ -0,0 +1,18 @@
+<footer class="footer bg-light">
+  <div
+    class="container-fluid d-flex flex-md-row flex-column justify-content-between align-items-md-center gap-1 container-p-x py-3">
+    <div>
+      <a
+        href="#"
+        target="_blank"
+        class="footer-text fw-bolder">CMS</a>
+      ©
+    </div>
+    <div>
+      <a href="#" class="footer-link me-4" target="_blank">License</a>
+      <a href="#" class="footer-link me-4">Help</a>
+      <a href="#" class="footer-link me-4">Contact</a>
+      <a href="#" class="footer-link">Terms &amp; Conditions</a>
+    </div>
+  </div>
+</footer>
---
+++ src/view/assets/components/navbar.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-example navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbar-ex-3">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbar-ex-3">
+      <div class="navbar-nav me-auto">
+        <a class="nav-item nav-link active" href="#">Home</a>
+        <a class="nav-item nav-link" href="#">About</a>
+        <a class="nav-item nav-link" href="#">Contact</a>
+      </div>
+
+      <form onsubmit="return false">
+        <button class="btn btn-outline-primary" type="button">Donate</button>
+      </form>
+    </div>
+  </div>
+</nav>
