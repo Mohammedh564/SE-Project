670f4a25879411900b868524e5b4e55e3daf1ac4
Mohammed <you@example.com>
2025-05-10 01:14:37 +0300

    database controller added

--- src/Controllers/DBControllor.php
+++ src/Controllers/DBControllor.php
@@ -1,13 +1,24 @@
-<?php
-class DBConroller {
-    public $dbHost = "localhost";
-    public $dbUser = "root";
-    public $dbPassword = "";
-    public $connection;
+<?php 
+class DBController { 
+    public $dbHost = "localhost"; 
+    public $dbUser = "root"; 
+    public $dbPassword = ""; 
+    public $dbName = ""; 
+    public $connection; 
+    
+    public function openConnection() { 
+        $this->connection = new mysqli(
+            $this->dbHost, 
+            $this->dbUser, 
+            $this->dbPassword, 
+            $this->dbName
+        ); 
 
-    public function openConnection (){
-        
-    }
-}
-
+        if ($this->connection->connect_error) { 
+            echo "Connection Error: " . $this->connection->connect_error; 
+            return false;
+        } 
+        return true;
+    } 
+} 
 ?>
